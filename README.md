eca7f93d32ef7b78dca676c8a716c1ef04c26ded
Franz Holzinger <franz.holzinger@festwein.de>
2023-05-06 19:06:38 +0200

    Support for TYPO3 12

--- README.md
+++ README.md
@@ -3,3 +3,22 @@ base_enum
 This provides the PHP Enum library.
 
 
+
+
+
+# TYPO3 extension base_enum
+
+## What is does
+
+This extension brings the "PHP Enum library" PHP classes "myclabs/php-enum" to TYPO3.
+
+## What is the origin
+
+See https://github.com/myclabs/php-enum/tree/1.8.4
+
+## Documentation
+
+https://github.com/myclabs/php-enum/tree/1.8.4
+
+
+
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext "base_svg".
+# Extension Manager/Repository config file for ext "base_enum".
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'PHP Enum Library',
     'description' => 'A library to make the PHP enum operator possible.',
     'category' => 'misc',
-    'version' => '0.0.2',
+    'version' => '1.8.4',
     'state' => 'stable',
     'clearcacheonload' => 1,
     'author' => 'PHP Enum contributors',
@@ -16,8 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'jambage.com',
     'constraints' => [
         'depends' => [
-            'php' => '7.3.0-8.1.99',
-            'typo3' => '9.5.0-11.5.99'
+            'typo3' => '10.4.0-12.3.99'
         ],
         'conflicts' => [
         ],
